582ad828a4c5b9cecf59c14dc9c3211ab7c43da2
Bridget Asser <bridget.asser@gmail.com>
2019-12-29 13:54:10 -0500

    data object, authorization, and JSON traversed data created

--- server.js
+++ server.js
@@ -16,19 +16,46 @@ app.post("/", function(req, res) {
   var lastName = req.body.lName;
   var email = req.body.email;
 
-  var option = {
-      url:
-  }
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-  request(options, function(error, response, body){
+  var jsonData = JSON.stringify(data);
 
-  })
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/30608749bd",
+    method: "POST",
+    headers: {
+      Authorization: "bill1 34de297b8fe2f759aca334aef8a91c8d-us4"
+    },
+    body: jsonData
+  };
 
-  console.log(firstName, lastName, email);
+  request(options, function(error, response, body) {
+    if (error) {
+      res.send("There was an error w signing up");
+    } else {
+      console.log(response.statusCode);
+    }
+  });
 });
 
 app.listen(3000, function() {
   console.log("Server is runnin on port 3000");
 });
 
+//API KEY
+// 34de297b8fe2f759aca334aef8a91c8d-us4
 
+//List ID
+
+// 30608749bd
